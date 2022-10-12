68141ee2fc0a86beaa3a100d8ec62eed43423091
Jwfelinto <jwfelinto@gmail.com>
2022-10-12 17:34:01 -0300

    Minha primeira aula de JavaScript

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*
+alert("meu primeiro js");
+var nome = "Jonathan Felinto";
+var idade = 33;
+var frase = "Seja bem vindo ";
+alert(frase + nome, " V");
+alert("Você tem " + idade + " anos.")*/
+
+//var lista= ["maçã", "laranja", "acerola", "maracuja"];
+//lista.push("uva");
+//lista.pop("");
+//alert(lista);
+
+//var fruta = [{nome:"maracujá", cor:"amarelo"} , {nome:"uva", cor:"roxa"}]
+//alert(fruta[1].nome);
+
+/*var idade = prompt("Qual sua idade?")
+//var idade =18
+if (idade>=18){
+    alert ("maior de idade");
+}else{
+    alert("menor de idade");
+}*/
+
+/*var count = 0;
+while (count <= 5){
+    alert(count);
+    count++;
+    
+}*/
+
+/*var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+}*/
+
+/*var d = new Date();
+alert(d); */
